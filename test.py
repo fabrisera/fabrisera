4106f1e67566ac53e9111298f8d5da590dd744e2
fabrisera <55074006+fabrisera@users.noreply.github.com>
2020-05-18 09:38:06 +0200

    general changes

--- test.py
+++
@@ -1,9 +0,0 @@
-while True:
-	a = input('Input first permutation: ')
-	b = input('Input second permutation: ')
-	if a.isdigit() or b.isdigit():
-		break
-	print("can only insert digits")	
-
-print(len(b))
-print(b)
