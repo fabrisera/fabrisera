13dd3ee9a3ad6e8862ff21a64111402d803ff54a
fabrisera <55074006+fabrisera@users.noreply.github.com>
2021-01-24 23:29:42 +0100

    ^^

--- learncpp1.2/learncpp1.2/main.cpp
+++ learncpp1.2/learncpp1.2/main.cpp
@@ -6,9 +6,14 @@
 //
 
 #include <iostream>
-
+// this program tests the direct list initialization which should be preferred
+// according to the tutorial
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//    direct list initialization of multiple variables separated with a comma
+    int a{5}, b{3};
+    a = 7;
+    // printing an expression and then a newline, would be cool to find a way to do both together!
+    printf("hello!");
+    std::cout << a;
     return 0;
-}
+} // i should just focus on working and that's it man!
--- python/Parenting Partnering Returns.py
+++ python/Parenting Partnering Returns.py
@@ -1,24 +1,35 @@
 t = int(input())
+#iterate over the test cases
 for t_case in range(t):
     times = []
     c_free = 0
     j_free = 0
-    result = ""
     var = True
     n = int(input())
+    #initialize the result list with n list of "C" which will be modified while iterating over the slots
+    result = ["C"] * n
     for i in range(n):
         times.append((list(map(int,input().strip().split()))))
+        #append the index in each array to keep track of the original position after sorting
+        times[i].append(i)
     times.sort()
+    #iterate over each slot and verity whether it can be assigned, else impossible and break
     for slot in times:
+        #if c is free then his name is already in the result list, just update the free will be time
         if slot[0] >= c_free:
-            result += "J"
             c_free = slot[1]
+        #if j is free then change the list within the result list from C to J and update the will be free time
         elif slot[0] >= j_free:
-            result += "C"
+            result[slot[2]] = "J"
             j_free = slot[1]
+        #if both c and j are already busy, then print IMPOSSIBLE and break   
         else:
             print("Case #{}: IMPOSSIBLE".format(t_case + 1))
             var = False
-            break        
+            break      
+    #if everything is possible then print the result      
     if var:
-        print("Case #{}: {}".format(t_case + 1, result))
+        string = ""
+        for char in result:
+            string += char
+        print("Case #{}: {}".format(t_case + 1, string))
